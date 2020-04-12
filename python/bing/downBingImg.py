41078da65c39fa523bb3189a8b0e24fe060228a1
linux <linux@zhangshuai.com>
2020-04-12 13:03:51 +0800

    change bing/downBingImg.py

--- python/bing/downBingImg.py
+++ python/bing/downBingImg.py
@@ -1,5 +1,6 @@
 import urllib.request as ur
+import datetime
 
 url = 'https://area.sinaapp.com/bingImg'
 html = ur.urlopen(url)
-open('bing.png','wb').write(html.read())
+open('bing/'+datetime.date.today().strftime()+'.png','wb').write(html.read())
