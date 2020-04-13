9013fbb0914bf2bc77524f60933942b261f8d63f
linux <linux@zhangshuai.com>
2020-04-13 16:21:49 +0800

    add jni

---
+++ cpp/jni/Main.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+  Created Time: 2020-04-13 16:17:37
+ ************************************************************************/
+public class Main
+{
+	native private int intMethod(int i, int j);
+	native public static String stringMethod(String str, int i);
+	public static void main(String[] args)
+	{
+		System.out.println("hello world!");
+	}
+}
---
+++ cpp/jni/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Created Time: 2020-04-13 16:20:14
+
+javac Main.java
+javah Main # 生成 Main.h
