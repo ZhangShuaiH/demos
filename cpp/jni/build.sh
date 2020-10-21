ae45e454bb44ace0660d6eabd7e23543ef507a41
张帅 <18143358+ZhangShuaiH@users.noreply.github.com>
2020-10-21 20:13:09 +0800

    Update build.sh

--- cpp/jni/build.sh
+++ cpp/jni/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # Created Time: 2020-04-13 16:20:14
 
-javac Main.java
+javac Main.java # 生成 class 文件
+
 javah Main # 生成 Main.h
+
+g++ -shared -fPIC main.cpp -o libmain.so # 生成 libmain.so 文件
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd` # 环境变量
+
+java Main # 执行
